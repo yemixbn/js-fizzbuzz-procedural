0131b5cb12666d398c4f4fe8c2b02b5525db51b5
faruq <yeemirahman@gmail.com>
2024-04-24 20:30:52 +0100

    completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,17 @@ const answer = []
 
 // Write your code below this line
 
-
-
-
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    answer.push('FizzBuzz')
+  } else if (i % 3 === 0) {
+    answer.push('Fizz')
+  } else if (i % 5 === 0) {
+    answer.push('Buzz')
+  } else {
+    answer.push(i)
+  }
+}
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
